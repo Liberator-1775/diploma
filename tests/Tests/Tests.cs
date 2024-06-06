3d7cbf437bf277834ee28ce9cc8aecb6939c8265
Nikita Ziganshin <nikita201721@gmail.com>
2024-06-06 03:29:30 +0300

    CI/CD на self-hosted с тестами готовы

--- tests/Tests/Tests.cs
+++ tests/Tests/Tests.cs
@@ -4,25 +4,42 @@ using OpenQA.Selenium.Support.UI;
 
 namespace Tests;
 
+[TestFixture]
 public class Tests
 {
     private ChromeDriver _driver;
     private WebDriverWait _wait;
+    private string _extensionId = string.Empty;
     
     [OneTimeSetUp]
     public void OneTimeSetUp()
     {
         Directory.SetCurrentDirectory("../../../../../chrome");
+        var options = new ChromeOptions();
+        options.AddExtension("../chrome.crx");
+        _driver = new ChromeDriver(options);
+        _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(30));
+        _driver.Navigate().GoToUrl("chrome://extensions/");
+        var developerToggle = (WebElement)_driver.ExecuteScript(
+            "return arguments[0].shadowRoot.getElementById(\"toolbar\").shadowRoot.getElementById(\"devMode\")",
+            _wait.Until(driver => driver.FindElement(By.TagName("extensions-manager"))));
+        developerToggle.Click();
+        var itemsContainer = (WebElement)_driver.ExecuteScript(
+            "return arguments[0].shadowRoot.getElementById(\"viewManager\").getElementsByClassName(\"active\")[0].shadowRoot.getElementById(\"content-wrapper\").getElementsByClassName(\"items-container\")[1]",
+            _wait.Until(driver => driver.FindElement(By.TagName("extensions-manager"))));
+        var extensionId = itemsContainer.FindElement(By.TagName("extensions-item"));
+        _extensionId = extensionId.GetAttribute("id");
+        _driver.Dispose();
     }
     
     [SetUp]
     public void SetUp()
     {
         var options = new ChromeOptions();
-        options.AddArgument($"load-extension={Directory.GetCurrentDirectory()}");
+        options.AddExtension("../chrome.crx");
         _driver = new ChromeDriver(options);
-        _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
-        _driver.Navigate().GoToUrl("chrome-extension://okicfnmkdafjfafmcgekmgioanbkffac/popup.html");
+        _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(20));
+        _driver.Navigate().GoToUrl($"chrome-extension://{_extensionId}/popup.html");
     }
     
     [Test]
@@ -102,6 +119,12 @@ public class Tests
 
     [TearDown]
     public void TearDown()
+    {
+        _driver.Dispose();
+    }
+
+    [OneTimeTearDown]
+    public void OneTimeTearDown()
     {
         _driver.Dispose();
     }
